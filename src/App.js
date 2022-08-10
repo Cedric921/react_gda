6ad096f1253395570d4e80e2f542d9fe217d66ba
cedric karungu <ckarungu921@gmail.com>
2022-08-10 16:29:25 +0200

    add cart

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 		<>
 			<Header />
 			<Main />
-			<Footer />
+			{/* <Footer /> */}
 		</>
 	);
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,52 @@
+import './Main.css';
+
+import githubImg from '../assets/github.svg';
+import fbImg from '../assets/facebook.png'
+import Linkendin from '../assets/LinkedIn.png'
+import Mille from '../assets/testimonials-5.jpg'
+import Mail from '../assets/gmail.webp'
 
 const Main = () => {
 	return (
 		<main>
-			<h1>hello from react</h1>
+			<div className='card'>
+				<div className='card__img'>
+					<img src={Mille} alt='' />
+				</div>
+				<div className='card__profil'>
+					<h2>Cedric karungu</h2>
+					<h4>Software developer</h4>
+					<p>ddgd ddhdd</p>
 
-			<ul>
-				<li>liste a puce 1</li>
-				<li>liste a puce 2</li>
-				<li>liste a puce 3</li>
-				<li>liste a puce 4</li>
-			</ul>
+					<button>
+						<img src={Mail} a lt='email' />
+						email
+					</button>
+				</div>
+				<div className='card__content'>
+					<h2 className='content__title'>About</h2>
+					<p className='card__text'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae
+					</p>
+				</div>
+				<div className='card__content'>
+					<h2 className='content__title'>About</h2>
+					<p className='card__text'>
+						libero ad vel eligendi ipsa nam numquam vitae provident aliquid,
+					</p>
+				</div>
+				<div className='card__footer'>
+					<div className='card__footer-img'>
+						<img src={githubImg} alt='fb' />
+					</div>
+					<div className='card__footer-img'>
+						<img src={fbImg} alt='fb' />
+					</div>
+					<div className='card__footer-img'>
+						<img src={Linkendin} alt='fb' />
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 };
