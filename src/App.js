7368abcf2ee644a6fa358844d81142b2e6f93de8
cedric karungu <ckarungu921@gmail.com>
2022-08-10 16:34:47 +0200

    fix import bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 
 import './App.css'
 
